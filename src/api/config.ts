17be3edd4bc80ac1be3dd71ad033583681107f7f
ramakrishnapakala <ramakrishnapakala25@gmail.com>
2024-11-12 20:35:42 +0530

    Modify Api Key

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 export const API_CONFIG = {
   BASE_URL: "https://api.openweathermap.org/data/2.5",
   GEO: "https://api.openweathermap.org/geo/1.0",
-  API_KEY: import.meta.env.VITE_OPENWEATHER_API_KEY,
+  API_KEY: "f8eca2a2fed3c251eaf773ad4544748f",
   DEFAULT_PARAMS: {
     units: "metric",
     appid: import.meta.env.VITE_OPENWEATHER_API_KEY,
